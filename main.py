69c9e75f16af818bf4b8ec543c615a0712dc9ef6
karimkohel <kareemkohel@gmail.com>
2019-09-08 18:45:54 +0200

    finished audio recognition function && tested it

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+########## Imports ##########
+
 import os
 import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+
+########## Fx ##########
 
 def say(text):
 	tts = gTTS(text=text, lang='en')
@@ -10,6 +14,22 @@ def say(text):
 	tts.save(filename)
 	playsound.playsound(filename)
 
+def get():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		said = ""
+
+		try:
+			said = r.recognize_google
+		except Exception as e:
+			print(" exception is : " + str(e))
+	return said
+
+
 
 # just testin the function and any runtime errors on my machine
 say("hello there")
+
+# fully functional
+get()
